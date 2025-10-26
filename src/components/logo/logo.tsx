d72c23f1da56c8ff6ce590ee88ea65d3d02dd77d
Daniel R Gorgonha <rogergorgonha@gmail.com>
2025-10-26 07:57:27 -0300

    feat: create Logo component and integrate it into Header and Footer

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,17 @@
+// import Image from "next/image"
+import Link from "next/link"
+
+export const Logo = () => {
+  return (
+    <Link href="/" title="Página inicial">
+      {/* <Image 
+        src="/logo.svg" 
+        alt="Logo site" 
+        width={100} 
+        height={100} 
+        className="w-10 h-10" 
+      /> */}
+      OConexo
+    </Link>
+  )
+}
